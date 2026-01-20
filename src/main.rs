8b76c00a71e94b9599cd8d7803bab7c71f39273b
deNoyersUgo <ugodenoyers@gmail.com>
2026-01-20 16:00:12 +0100

    monte carlo pricing implented in EuropeanOption. next to do is main

--- src/main.rs
+++ src/main.rs
@@ -29,23 +29,44 @@ impl EuropeanOption {
         let sigma_sqrt_t = self.volatility * self.maturity.sqrt();
 
         //d_+ computation
-        let dplus = ((self.spot_price / self.strike_price).ln()
+        let d_plus = ((self.spot_price / self.strike_price).ln()
             + (self.risk_free + 0.5 * self.volatility.powi(2)) * self.maturity)
             / sigma_sqrt_t;
 
         //d_- computation
-        let dminus = dplus - sigma_sqrt_t;
+        let d_minus = d_plus - sigma_sqrt_t;
 
         match self.option_type {
             OptionType::Call => {
-                // Call = S * N(dplus) - K * e^(-rT) * N(dminus)
-                self.spot_price * n.cdf(dplus) - self.strike_price * E.powf(-self.risk_free * self.maturity) * n.cdf(dminus)
+                // Call = S * N(d_plus) - K * e^(-rT) * N(d_minus)
+                self.spot_price * n.cdf(d_plus) - self.strike_price * E.powf(-self.risk_free * self.maturity) * n.cdf(d_minus)
             }
             OptionType::Put => {
-                // Put = K * e^(-rT) * N(-dminus) - S * N(-dplus)
-                self.strike_price * E.powf(-self.risk_free * self.maturity) * n.cdf(-dminus) - self.spot_price * n.cdf(-dplus)
+                // Put = K * e^(-rT) * N(-d_minus) - S * N(-d_plus)
+                self.strike_price * E.powf(-self.risk_free * self.maturity) * n.cdf(-d_minus) - self.spot_price * n.cdf(-d_plus)
             }
         }
+    }
+
+    pub fn price_mc(&self, num_sims: usize) -> f64 {
+        let drift = (self.risk_free - 0.5 * self.volatility.powi(2)) * self.maturity;
+        let diffusion = self.volatility * self.maturity.sqrt();
+        let discount_factor = E.powf(-self.risk_free * self.maturity);
+
+        let sum_payoffs: f64 = (0..num_sims).into_par_iter().map(|_| {
+            let mut rng = rand::thread_rng();
+
+            let z: f64 = rng.sample(StandardNormal);
+
+            let s_t = self.spot_price * (drift * diffusion * z).exp();
+
+            match self.option_type {
+                OptionType::Call => {s_t - self.strike_price}.max(0.0),
+                OptionType::Put => {self.strike_price - s_t}.max(0.0),
+            }
+        }).sum();
+
+        (sum_payoffs / num_sims as f64) * discount_factor
     }
 }
 
