df5e023f88a9423bf20cd5a7b250735e56f04d6a
deNoyersUgo <ugodenoyers@gmail.com>
2026-01-20 14:47:54 +0100

    black scholes pricing implented in EuropeanOption. next implementation is monte carlo one

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,53 @@
-pub struct BlackScholesModel {
-    risk_free: f64 // risk-free rate
-}
+use rand::prelude::*;
+use rand_dist::StandardNormal;
+use rayon::prelude::*;
+use stars::distribution::{ContinuousCDF, Normal};
+use std::f64::consts::E;
+use sdt::time::Instant;
 
-impl BlackScholesModel {
-    pub fn new(risk_free: f64) -> Self{
-        BlackScholesModel {risk_free}
-    }
+pub enum OptionType {
+    Call,
+    Put
 }
-
 pub struct EuropeanOption {
+    spot_price: f64,
     strike_price: f64,
+    risk_free: f64,
+    volatility: f64,
     maturity: f64,
-    is_call: bool,
+    option_type: OptionType,
 }
 
 impl EuropeanOption {
-    fn new(strike_price: f64, maturity: f64, is_call: bool) -> Self {
-        EuropeanOption {strike_price, maturity, is_call}
-    }
-}
-
-pub struct MonteCarloEngine {
-    num_paths: usize,
-    num_steps: usize,
-}
-
-impl MonteCarloEngine {
-    pub fn new(num_paths: usize, num_steps: usize) -> Self{
-        MonteCarloEngine { num_paths, num_steps }
+    fn new(spot_price: f64, strike_price: f64, risk_free: f64, volatility: f64, maturity: f64, option_type: OptionType) -> Self {
+        EuropeanOption { spot_price, strike_price, risk_free, volatility, maturity, option_type }
     }
 
-    pub fn price_option(&self, model: &BlackScholesModel, option: &EuropeanOption, s0: f64) -> f64 {
+    pub fn price_bs(&self) -> f64 {
+        let n = Normal::new(0.0, 1.0).unwrap();
 
+        let sigma_sqrt_t = self.volatility * self.maturity.sqrt();
+
+        //d_+ computation
+        let dplus = ((self.spot_price / self.strike_price).ln()
+            + (self.risk_free + 0.5 * self.volatility.powi(2)) * self.maturity)
+            / sigma_sqrt_t;
+
+        //d_- computation
+        let dminus = dplus - sigma_sqrt_t;
+
+        match self.option_type {
+            OptionType::Call => {
+                // Call = S * N(dplus) - K * e^(-rT) * N(dminus)
+                self.spot_price * n.cdf(dplus) - self.strike_price * E.powf(-self.risk_free * self.maturity) * n.cdf(dminus)
+            }
+            OptionType::Put => {
+                // Put = K * e^(-rT) * N(-dminus) - S * N(-dplus)
+                self.strike_price * E.powf(-self.risk_free * self.maturity) * n.cdf(-dminus) - self.spot_price * n.cdf(-dplus)
+            }
+        }
     }
 }
-
 
 fn main() {
     println!("Hello, world!");
